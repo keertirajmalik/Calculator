e4f369c6c514be54e43305494f799f91567eeda4
keertirajmalik <keertirajmalik@gmail.com>
2021-12-21 22:13:42 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Keertiraj Laxman Malik on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
